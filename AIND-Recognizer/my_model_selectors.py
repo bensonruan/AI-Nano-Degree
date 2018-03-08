8ef548a239dfe18dc6ea96e34b911dac3c1a4001
Benson Ruan <benson_ruan@hotmail.com>
2018-03-08 21:17:36 +1100

    Part 2 Model selection

--- AIND-Recognizer/my_model_selectors.py
+++ AIND-Recognizer/my_model_selectors.py
@@ -34,7 +34,7 @@ class ModelSelector(object):
     def base_model(self, num_states):
         # with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-        # warnings.filterwarnings("ignore", category=RuntimeWarning)
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
         try:
             hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
                                     random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
@@ -76,8 +76,22 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # implement model selection based on BIC scores
+        best_score = float("inf")
+        best_model = None
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model = self.base_model(n)
+                p = n * (n-1) + 2 * len(self.X[0]) * n
+                logL = model.score(self.X, self.lengths)
+                logN = np.log(len(self.X))
+                score = -2 * logL + p * logN
+                if score < best_score:
+                    best_score = score
+                    best_model = model
+            except:
+                continue
+        return best_model
 
 
 class SelectorDIC(ModelSelector):
@@ -93,9 +107,36 @@ class SelectorDIC(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # implement model selection based on DIC scores
+        best_score = -float("inf")
+        best_n = None
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                logPXi = self.base_model(n).score(self.X, self.lengths)
 
+                sumLogPX = 0.
+                words = list(self.words.keys())
+                M = len(words)
+                words.remove(self.this_word)
+
+                for word in words:
+                    try:
+                        all_but_i = ModelSelector(self.words, self.hwords, word, self.n_constant, self.min_n_components, self.max_n_components, self.random_state, self.verbose)
+                        sumLogPX += all_but_i.base_model(n).score(all_but_i.X, all_but_i.lengths)
+                    except:
+                        M = M - 1
+
+                DIC = logPXi - sumLogPX / (M - 1)
+
+                if best_score is None or best_score < DIC:
+                  best_score, best_n = DIC, n
+            except:
+                pass
+
+        if best_n is None:
+            return self.base_model(self.n_constant)
+        else:
+            return self.base_model(best_n)
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
@@ -105,5 +146,31 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        # implement model selection using CV
+        best_score = -float("inf")
+        best_num_state = 1
+        best_model = None
+
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            i = 0
+            total_score = 0
+            word_sequences = self.sequences
+            try:
+                kfold_idx = KFold(n_splits=min(3, len(word_sequences)))
+            except:
+                return None
+            try:
+                for cv_train_idx, cv_test_idx in kfold_idx.split(word_sequences):
+                    cv_train_x, cv_train_length = combine_sequences(cv_train_idx, word_sequences)
+                    cv_test_x, cv_test_length = combine_sequences(cv_test_idx, word_sequences)
+                    model = GaussianHMM(n_components=n, covariance_type="diag", n_iter=1000,
+                                        random_state=self.random_state, verbose=False).fit(cv_train_x, cv_train_length)
+                    total_score = total_score + model.score(cv_test_x, cv_test_length)
+                    i = i + 1
+                if total_score / i > best_score:
+                    best_score = total_score / i
+                    best_num_state = n
+            except:
+                continue
+        best_model = self.base_model(best_num_state)
+        return best_model
